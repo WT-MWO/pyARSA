55ece302ff9a1fb3553c105b223e798929b1320d
Marcin Wolinski <mwo@woodthilsted.com>
2023-06-20 16:11:56 +0200

    String docs added.

--- src/Structure.py
+++ src/Structure.py
@@ -7,6 +7,11 @@ from RobotOM import *
 
 class Structure:
     def __init__(self, app):
+        """
+        This is Python wrapper for Autodesk Robot API.
+        Parameters:
+            app(RobotApplication): Robot application
+        """
         self.app = app
         self.project = self.app.Project
         self.structure = self.project.Structure
@@ -15,10 +20,19 @@ class Structure:
         self.nodes = self.structure.Nodes
 
     def get_all_nodes(self):
+        """
+        Gets all nodes in the Roboit Model
+        Returns: IRobotCollection of nodes
+        """
         return self.nodes.GetAll()
     
     def get_node_from_collection(self, index):
+        """
+        Returns the node of given index.
+        Parameters:
+        index(int): index of node
+        Returns: IRobotNode object
+        """
         node_collection = self.get_all_nodes()
         node = IRobotNode(node_collection.Get(index))
         return node
-
