e2ba28212e47986a41fec5596642d2088cb3249c
Marcin Wolinski <mwo@woodthilsted.com>
2024-03-18 11:06:42 +0100

    Screenshot function implemented

--- src/save_screenshot.py
+++ src/save_screenshot.py
@@ -2,15 +2,14 @@ import win32clipboard
 from io import BytesIO
 from PIL import Image
 
-# This code to be fixed and tested - draft created by ChatGPT
 
-
-def get_image_from_clipboard():
+def _get_image_from_clipboard():
     win32clipboard.OpenClipboard()
     try:
         if win32clipboard.IsClipboardFormatAvailable(win32clipboard.CF_ENHMETAFILE):
             data = win32clipboard.GetClipboardData(win32clipboard.CF_ENHMETAFILE)
-            return BytesIO(data)
+            im = Image.open(BytesIO(data))
+            return im
         elif win32clipboard.IsClipboardFormatAvailable(win32clipboard.CF_METAFILEPICT):
             data = win32clipboard.GetClipboardData(win32clipboard.CF_METAFILEPICT)
             metafile_picture = data[
@@ -28,15 +27,28 @@ def get_image_from_clipboard():
         win32clipboard.CloseClipboard()
 
 
-def save_image_as_emf(image, filename):
+def _save_image(image, filename):
     if isinstance(image, Image.Image):
-        image.save(filename, "EMF")
+        image.save(filename)
         print(f"Image saved as {filename}")
     else:
         print("Not a valid image.")
 
 
+
+def save_screenshot(path, name):
+    """Saves the ARSA model screenshot from clipboars as png.
+    Parameters:
+    path(string): path to directory where you want to save
+    filename(string): filename of your screenshot"""
+    image = _get_image_from_clipboard()
+    full_name = path+ '\\'+name+'.png'
+    _save_image(image, full_name)
+
+
+
 if __name__ == "__main__":
-    image = get_image_from_clipboard()
-    if image:
-        save_image_as_emf(image, "clipboard_image.emf")
+    path = "C:\\Users\\mwo\\OneDrive - WoodThilsted Partners\\Professional\\5_PYTHON\\pyARSA\\tests\\"
+    name = "test"
+    save_screenshot(path=path, name=name)
+
